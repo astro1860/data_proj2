8baa63d06958e545ad6ba0524674c03864dbcf10
Lulu <luhangxin@gmail.com>
2015-11-06 18:19:26 +0100

    Update mapper.py
    new parameters

--- mapper.py
+++ mapper.py
@@ -23,8 +23,21 @@ def transform(x_original):
 
 if __name__ == "__main__":
     #f = open('/Users/Hangxin/Desktop/training_set.txt')
-    clf = SGDClassifier(loss='hinge', penalty='l2',alpha=0.0001, l1_ratio=0.15, fit_intercept=False, n_iter=1, shuffle=True, verbose=0, epsilon=0.1, n_jobs=1, random_state=1, learning_rate='optimal', eta0=0.0, power_t=0.5, class_weight=None, warm_start=False, average=False)
+    clf = SGDClassifier(loss='hinge', penalty='l1',alpha=0.0001/15, l1_ratio=0.15, fit_intercept=False, n_iter=1, shuffle=True, verbose=0, epsilon=0.1, n_jobs=1, random_state=1, learning_rate='optimal', eta0=0.0, power_t=0.5, class_weight=None, warm_start=False, average=True)
     #Extract Features
+    #alpha = 0.0001 = a l1_ratio = 0.15
+    #0.000001 --> 0.779509  a/100
+    #0.00001 --> 0.813194   a/10
+    #0.0001/20 --> 0.811584 a/20
+    #0.0001/15 --> 0.813287 a/15 !!
+    #0.0001/12-->0.807602 a/12
+    #0.0001/17 --> 0.811538
+
+    #0.814882 <-- L1_RATION 0.5 L1
+    #0.819020 <-- l1 RATION 0.5 L1 average = true alpha = a/15 !!
+    #0.817806 <-- l2 ration 0.15 average = true alpha = a/15
+    #0.818444 <-- l2 ration 0.15 average  =true alpha = a/100
+    #0.818413 <-- l2 ration 0.15 average = true alpha = a/50 0.819020
     index = 0
     for line in sys.stdin:
         line = line.strip()
